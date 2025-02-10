a0f50fff19e5d5e7c0c5b19b133d77e3a10133fa
Hung Bui <quochungb3@gmail.com>
2025-02-10 21:28:49 +0700

    Hung:

--- Day1/var-demo.js
+++ Day1/var-demo.js
@@ -8,3 +8,13 @@ console.log("x ben ngoai ham:",x);
 
 // Thực thi hàm
 varDemo();
+
+// Local
+function fn_demo(){
+    var y=200; //local
+    console.log("y ben trong ham:");
+}
+fn_demo();
+//y ben ngoai ham
+console.log("y ben ngoai ham:",y); // ==error
+
